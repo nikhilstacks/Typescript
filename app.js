a867d8f7d8e95dddaf8277fe6e13a5178780478c
Kumar <nikhil.kumar@duckcreek.com>
2023-09-24 23:53:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var fname = 'nikhil';
+var Myself = {
+    name: 'Nikhil',
+    age: 22,
+    designation: 'Software configuration specialist'
+};
+console.log("This is the first typescript and my name is ".concat(fname, " and this is my data ").concat(Myself));
---
+++ app.ts
@@ -0,0 +1,14 @@
+var fname:string = 'nikhil'
+
+let Myself : {
+    name: string,
+    age: number,
+    designation: string
+} = {
+    name: 'Nikhil',
+    age: 22,
+    designation: 'Software configuration specialist'
+}
+
+
+console.log(`This is the first typescript and my name is ${fname} and this is my data ${Myself}`)
